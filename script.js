370d60ed9cd3b00d45f1c5352635387d244e4cdb
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-02-02 01:40:03 +0100

    commit

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let MenuOp = document.querySelector(".menu-open");
 let MenuCl = document.querySelector(".menu-close");
 let MenuEl = document.querySelector(".menu");
 
-MenuOp.addEventListener('click', function(){
-      MenuCl.classList.remove('hidden');
-      MenuEl.classList.remove('max-lg:hidden');
-      MenuOp.classList.add('hidden');
-    
-  });
+MenuOp.addEventListener('click', function () {
+  MenuCl.classList.remove('hidden');
+  MenuEl.classList.remove('max-lg:hidden');
+  MenuOp.classList.add('hidden');
 
-  MenuCl.addEventListener('click', function(){
-    MenuCl.classList.add('hidden');
-    MenuEl.classList.add('max-lg:hidden');
-    MenuOp.classList.remove('hidden');
+});
+
+MenuCl.addEventListener('click', function () {
+  MenuCl.classList.add('hidden');
+  MenuEl.classList.add('max-lg:hidden');
+  MenuOp.classList.remove('hidden');
 });
 
 const questions = document.querySelectorAll("dt");
@@ -22,16 +22,19 @@ const arrows = document.querySelectorAll(".arrow");
 questions.forEach((question, index) => {
   question.addEventListener("click", () => {
 
-      answers.forEach((answer) => {
-        answer.classList.add("hidden");
-        answer.classList.remove("grid");  
-      });
-      answers[index].classList.remove("hidden");
-      answers[index].classList.add("grid");
+    answers.forEach((answer) => {
+      answer.classList.add("hidden");
+      answer.classList.remove("grid");
+    });
+    
+    answers[index].classList.remove("hidden");
+    answers[index].classList.add("grid");
 
     arrows.forEach((arrow) => {
-      arrow.classList.remove("active"); 
+      arrow.classList.remove("active");
     });
-    arrows[index].classList.toggle("active");
+
+      arrows[index].classList.toggle("active");
+    
   });
 });
